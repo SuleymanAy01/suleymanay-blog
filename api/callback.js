95765b6528d0b5f7243d36c10805043580c61b87
Süleyman Ay <06suleymanay@gmail.com>
2026-08-20 13:54:03 +0300

    Create callback.js

---
+++ api/callback.js
@@ -0,0 +1,50 @@
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  if (!code) {
+    return res.status(400).send('Kod bulunamadı');
+  }
+
+  try {
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: process.env.OAUTH_CLIENT_ID,
+        client_secret: process.env.OAUTH_CLIENT_SECRET,
+        code,
+      }),
+    });
+
+    const data = await response.json();
+    const token = data.access_token;
+
+    const html = `
+      <!DOCTYPE html>
+      <html>
+        <body>
+          <script>
+            (function() {
+              function receiveMessage(e) {
+                window.opener.postMessage(
+                  'authorization:github:success:${JSON.stringify({ token, provider: 'github' })}',
+                  e.origin
+                );
+              }
+              window.addEventListener("message", receiveMessage, false);
+              window.opener.postMessage("authorizing:github", "*");
+            })();
+          </script>
+        </body>
+      </html>
+    `;
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.status(200).send(html);
+  } catch (error) {
+    res.status(500).send('OAuth hatası oluştu');
+  }
+}

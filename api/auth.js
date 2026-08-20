e669523f683d10b666e294d7736240feb8f56dc0
Süleyman Ay <06suleymanay@gmail.com>
2026-08-20 13:53:30 +0300

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  const clientId = process.env.OAUTH_CLIENT_ID;
+  const redirectUri = 'https://www.suleymanay.com/api/callback';
+  
+  const githubUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo`;
+  
+  res.redirect(302, githubUrl);
+}

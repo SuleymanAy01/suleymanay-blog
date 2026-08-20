7dba7c0e5310cae3abd1e5b7d9d4a3ce412a9116
Süleyman Ay <06suleymanay@gmail.com>
2026-08-20 13:49:12 +0300

    Update and rename auth.ts to auth.js

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,10 +1,10 @@
-import type { APIRoute } from 'astro';
+export const prerender = false;
 
-export const GET: APIRoute = async ({ redirect }) => {
+export async function GET({ redirect }) {
   const clientId = process.env.OAUTH_CLIENT_ID;
   const redirectUri = 'https://www.suleymanay.com/api/callback';
   
   const githubUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo`;
   
   return redirect(githubUrl, 302);
-};
+}
